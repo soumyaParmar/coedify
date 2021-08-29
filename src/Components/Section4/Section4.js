b755db72bfa08c6ee32706b34363f59577bbd18e
Nishant Agarwal <82943332+nishant803@users.noreply.github.com>
2021-08-29 09:16:31 -0700

    Cards fit

--- src/Components/Section4/Section4.js
+++ src/Components/Section4/Section4.js
@@ -3,6 +3,18 @@ import DevCard from "./DevCard";
 import { DevList } from "./DevList";
 
 const Section4 = () => {
+  const list1 = DevList.map(({id, url, title, item1, item2, item3})=>{
+    return (
+      <DevCard
+              index={id}
+              url={url}
+              title={title}
+              item1={item1}
+              item2={item2}
+              item3={item3}
+            />
+    )
+  })
   return (
     <div className="igns">
       <h1 className="ings__heading">Our Ingenious Developers</h1>
@@ -15,25 +27,14 @@ const Section4 = () => {
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation{" "}
       </p>
       <div className="dev__sections">
-        {DevList.map(({ id, url, title, item1, item2, item3 }) => {
-          return (
-            <DevCard
-              index={id}
-              url={url}
-              title={title}
-              item1={item1}
-              item2={item2}
-              item3={item3}
-            />
-
-          );
-        })}
+      {list1.splice(0,3)}
       </div>
       <div className="grad">
-     
         <h1 className="grad__heading">Meet the best developers on CoEdify as per your requirements</h1>
         <button className="btn__getDeveloperGrad">Hire Developers</button>
-     
+      </div>
+      <div className="dev__sections">
+      {list1.splice(0,3)}
       </div>
     </div>
   );
