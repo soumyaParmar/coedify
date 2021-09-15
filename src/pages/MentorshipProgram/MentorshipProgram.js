facb1f5e89f2fb2a99f459246f8e2f08bb81381c
Kunal Pandey <pandeykunal2097@gmail.com>
2021-09-16 01:23:01 +0530

    Completed Program-Page Image Section

--- src/pages/MentorshipProgram/MentorshipProgram.js
+++ src/pages/MentorshipProgram/MentorshipProgram.js
@@ -2,12 +2,14 @@ import React from 'react'
 import "./MentorshipProgram.css"
 import ProgramPageHeader from '../../Components/ProgramPageHeader/ProgramPageHeader'
 import ProgramPageSection from '../../Components/ProgramPageSection/ProgramPageSection'
+import ProgramPageImageSection from '../../Components/ProgramPageImageSection/ProgramPageImageSection'
 
 const MentorshipProgram = () => {
     return (
         <div className='page-body'>
             <ProgramPageHeader/>
             <ProgramPageSection/>
+            <ProgramPageImageSection/>
         </div>
     )
 }
