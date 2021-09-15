bace74fae74f3875a8a39cb42caa3b2ad54a0e4b
Nishant Agarwal <82943332+nishant803@users.noreply.github.com>
2021-09-15 17:37:06 +0530

    Technology Card

--- src/Components/TechnologyPageSection/TechCard.jsx
+++ src/Components/TechnologyPageSection/TechCard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "../Section4/DevCard.css";
+import "./TechCard.css";
 function TechCard({ id, url, title, item1, item2, item3 }) {
   return (
     <>
-      <div className="dev__box">
+      <div className="tech__box">
         {/* <div className="dev__content"> */}
         <img className="dev__img" src={url} alt="fullstack" />
         <h4 className="dev__heading">
